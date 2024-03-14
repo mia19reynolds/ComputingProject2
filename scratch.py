1678de0d57bd98319d5254922a923e95b877006a
antonvid <antonyvidler@gmail.com>
2024-03-14 12:08:03 +0000

    updated requirements.txt

--- scratch.py
+++ scratch.py
@@ -60,7 +60,7 @@ def load_user(email):
         print("User not found") 
         return None
 
-@app.route('/index', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         return render_template('index.html')
