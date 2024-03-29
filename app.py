18f550f5de817df130a956cd368358e02dbd4adf
antonvid <antonyvidler@gmail.com>
2024-03-29 17:47:52 +0000

    changed host to 0.0.0.0

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ def logout():
         return render_template('logout.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=80, use_reloader=False)
 
 
 # # Dynamic recipe options
