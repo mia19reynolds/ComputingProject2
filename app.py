554b222b7417f8a724f35d75fbfc36cd2f78e50e
antonvid <antonyvidler@gmail.com>
2024-03-30 21:53:17 +0000

    fixed intolerances not showing newly selected options

--- app.py
+++ app.py
@@ -369,9 +369,7 @@ def settings():
 @login_required
 def settingsIntolerances():
     activeUser = current_user.id
-    selectedIntolerances = readDatabase("intolerances", "user_data", "email", activeUser)
     intolerances = [
-        'Dairy',
         'Dairy',
         'Egg',
         'Gluten',
@@ -392,8 +390,10 @@ def settingsIntolerances():
         cursor.execute("UPDATE user_data SET intolerances = %s WHERE email = %s", (newIntolerancesString, activeUser))
         db.commit()
         cursor.close()
+        selectedIntolerances = readDatabase("intolerances", "user_data", "email", activeUser)
         return render_template('intolerances.html', items=intolerances, checked=selectedIntolerances, alert='Intolerances updated')
     elif request.method == 'GET':
+        selectedIntolerances = readDatabase("intolerances", "user_data", "email", activeUser)
         return render_template('intolerances.html', items=intolerances, checked=selectedIntolerances)
 
 @app.route('/settings/diets', methods=['GET', 'POST'])
