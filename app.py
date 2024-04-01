23e9fc789f049ee7c2ba6243d019a20bec62c3d8
antonvid <antonyvidler@gmail.com>
2024-04-01 17:19:38 +0100

    updated app.py to use Flask-MySQL

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ api_key = "b1950d16d34842d6be06bca4c29ea1fb"
 
 mysql = MySQL()
 mysql.init_app(app)
-db = mysql.get_db()
 
 # HOST = "localhost"
 # DATABASE = "Gyaldem"
@@ -248,9 +247,9 @@ def signup():
 
                 else:
                     hashed_password = generate_password_hash(password).decode('utf-8')
-                    cursor = db.cursor()
+                    cursor = mysql.get_db().cursor()
                     cursor.execute("INSERT INTO users (forename, lastname, email, password) VALUES (%s, %s, %s, %s)", (forename, lastname, email, hashed_password))
-                    db.commit()
+                    mysql.get_db().commit()
                     cursor.close()
                     # Log in the user after signup
                     user = User(email=email, forename=forename)
@@ -272,7 +271,7 @@ def signup():
 
 # Check if login exists
 def login_exists(email):
-    cursor = db.cursor()
+    cursor = mysql.get_db().cursor()
     try:
         count = readDatabase('COUNT(*)', 'users', 'email', email)
         if count:
@@ -282,7 +281,7 @@ def login_exists(email):
         cursor.close()
 
 def check_password(email, password):
-    # cursor = db.cursor()
+    # cursor = mysql.get_db().cursor()
     try:
         hashed_password = readDatabase('password', 'users', 'email', email)
         
@@ -396,9 +395,9 @@ def settingsIntolerances():
     if request.method == 'POST':
         newIntolerances = request.form.getlist('checkbox')
         newIntolerancesString = ','.join(newIntolerances)
-        cursor = db.cursor()
+        cursor = mysql.get_db().cursor()
         cursor.execute("UPDATE user_data SET intolerances = %s WHERE email = %s", (newIntolerancesString, activeUser))
-        db.commit()
+        mysql.get_db().commit()
         cursor.close()
         selectedIntolerances = readDatabase("intolerances", "user_data", "email", activeUser).split(',')
         return render_template('intolerances.html', items=intolerances, checked=selectedIntolerances, alert='Intolerances updated')
@@ -426,9 +425,9 @@ def settingsDiets():
     if request.method == 'POST':
         newDiets = request.form.getlist('checkbox')
         newDietsString = ','.join(newDiets)
-        cursor = db.cursor()
+        cursor = mysql.get_db().cursor()
         cursor.execute("UPDATE user_data SET diets = %s WHERE email = %s", (newDietsString, activeUser))
-        db.commit()
+        mysql.get_db().commit()
         cursor.close()
         selectedDiets = readDatabase("diets", "user_data", "email", activeUser).split(',')
         return render_template('diets.html', items=diets, checked=selectedDiets, alert='Diets updated')
@@ -448,9 +447,9 @@ def settingsResetPassword():
         if check_password(activeUser, oldPass):
             if newPass == newPassConfirm:
                 hashPass = generate_password_hash(newPass).decode('utf-8')
-                cursor = db.cursor()
+                cursor = mysql.get_db().cursor()
                 cursor.execute("UPDATE users SET password = %s WHERE email = %s", (hashPass, activeUser))
-                db.commit()
+                mysql.get_db().commit()
                 cursor.close()
                 return render_template('resetPassword.html', alert='Password changed')
             else:
@@ -464,7 +463,7 @@ def readDatabase(reqCol, table, column, value):
     print('read:', reqCol, table, column, value)
     try:
         query = 'SELECT {} FROM {} WHERE {} = %s'.format(reqCol, table, column)
-        cursor = db.cursor()
+        cursor = mysql.get_db().cursor()
         cursor.execute(query, (value,))
         result = cursor.fetchone()[0]
         cursor.close()
@@ -477,7 +476,7 @@ def writeDatabase(table, columns, values):
     try:
         query = 'INSERT INTO {} ({}) VALUES ({})'.format(table, columns, values)
         print('write:', query)
-        cursor = db.cursor()
+        cursor = mysql.get_db().cursor()
         cursor.execute(query)
         result = cursor.fetchone()
         cursor.close()
