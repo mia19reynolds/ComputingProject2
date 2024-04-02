5c346ddc9964372b73a76aad33e0333d98fea3de
antonvid <antonyvidler@gmail.com>
2024-04-02 01:42:59 +0100

    troubleshooting dropdown.js

--- static/scripts/dropdown.js
+++ static/scripts/dropdown.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for(var i=0; i<dropdowns.length; i++){
         dropdowns[i].getElementsByClassName('anchor')[0].onclick = function(evt) {
             console.log('clicked');
+            console.log(dropdowns[i].classList)
     
             if (dropdowns[i].classList.contains('visible')){
                 dropdowns[i].classList.remove('visible');
