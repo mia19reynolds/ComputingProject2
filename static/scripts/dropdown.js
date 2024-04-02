79a2c76ca3a3a38cd9f3d6c40b6dabe70f23aa69
antonvid <antonyvidler@gmail.com>
2024-04-02 01:53:13 +0100

    dropdown fix?

--- static/scripts/dropdown.js
+++ static/scripts/dropdown.js
@@ -3,16 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     var dropdowns = document.getElementsByClassName('dropdown');
     for(var i=0; i<dropdowns.length; i++){
         dropdowns[i].getElementsByClassName('anchor')[0].onclick = function(evt) {
-            console.log('clicked');
-            console.log(dropdowns[i].classList)
-    
-            if (dropdowns[i].classList.contains('visible')){
-                dropdowns[i].classList.remove('visible');
-            }else{
-                dropdowns[i].classList.add('visible');
-            }
+            dropdown(i);
         }
     }
     
+    function dropdown(i){
+        console.log('clicked');
+        console.log(dropdowns[i].classList)
+
+        if (dropdowns[i].classList.contains('visible')){
+            dropdowns[i].classList.remove('visible');
+        }else{
+            dropdowns[i].classList.add('visible');
+        }
+    }
 
 });
